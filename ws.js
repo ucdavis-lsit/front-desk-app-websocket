fc063de9e66a8ddd1aec334883613b3ba3fccf08
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2022-01-20 14:11:28 -0800

    clean up and ping

--- ws.js
+++ ws.js
@@ -1,8 +1,79 @@
+require('dotenv').config()
 const express = require('express');
 const { route } = require('express/lib/application');
 const ws = require('ws');
+const url = require('url');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser')
 
+const jwtSecret = process.env.JWT_SECRET;
+
+// Express app
 const app = express();
+app.use(bodyParser.json({
+    type: "*/*"
+}));
+
+// Initiate WebSocket server and listen on port 80 at /ws via express
+const wss = new ws.Server({
+	noServer: true,
+	path: "/ws",
+	clientTracking: true
+})
+
+server = app.listen(80);
+server.on('upgrade', (request, socket, head) => {
+	wss.handleUpgrade(request, socket, head, socket => {
+		wss.emit('connection', socket, request);
+	});
+});
+
+wss.on('connection', function connection(ws, req) {
+	var token = url.parse(req.url, true).query.token;
+	jwt.verify(token, jwtSecret, (err, decoded) => {
+        if (err) {
+            console.log("Invalid JWT")
+			ws.terminate()
+        } else {
+			if(!decoded.user_id){
+				console.error("JWT must contain user_id")
+				ws.terminate()
+			} else{
+				ws.user_id = decoded.user_id;
+			}
+			
+        }
+    });
+
+	ws.on('close', function close() {
+		console.log('websocket closed');
+		//TODO more helpful logging
+	});
+	
+	ws.on('message', function message(data) {
+		console.log('WSS Recieved', data);
+		//TODO remove if clients cant talk back
+	});
+	
+	ws.on('pong', function pong(){
+		console.log("pong")
+		this.isAlive = true;
+	});
+});
+
+// Check connections every 30 seconds
+const interval = setInterval(function ping() {
+	wss.clients.forEach(function each(ws) {
+	  if (ws.Alive === false) return ws.terminate();
+  
+	  ws.isAlive = false;
+	  ws.ping();
+	  console.log("ping")
+	});
+  }, 30000);
+
+
+// Express routes
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -14,59 +85,9 @@ router.get('/', (req, res) => {
   res.status(200).send('Ok');
 });
 
-var clients = new Array;
-
-
-function handleConnection(client, request) {
-	console.log("New Connection");        // you have a new client
-	clients.push(client);    // add this client to the clients array
-
-	function endClient() {
-		// when a client closes its connection
-		// get the client's position in the array
-		// and delete it from the array:
-		var position = clients.indexOf(client);
-		clients.splice(position, 1);
-		console.log("connection closed");
-	}
-
-	// if a client sends a message, print it out:
-	function clientResponse(data) {
-		console.log(request.connection.remoteAddress + ': ' + data);
-		broadcast(request.connection.remoteAddress + ': ' + data);
-	}
-
-	// set up client event listeners:
-	client.on('message', clientResponse);
-	client.on('close', endClient);
-  client.on('error', () => console.log('errored'));
-}
-
-// This function broadcasts messages to all webSocket clients
-function broadcast(data) {
-	// iterate over the array of clients & send data to each
-	for (c in clients) {
-		clients[c].send(JSON.stringify(data));
-	}
-}
-
-// listen for clients and handle them:
-
-// Set up a headless websocket server that prints any
-// events that come in.
-const wsServer = new ws.Server({
-	noServer: true,
-	path: "/ws"
+router.post('/messages', (req, res) => {
+	console.log("POST to messages",req.body);
+	res.status(200).send('Ok');
 });
-wsServer.on('connection', handleConnection);
 
-// `server` is a vanilla Node.js HTTP server, so use
-// the same ws upgrade process described here:
-// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
-const server = app.listen(8080);
-server.on('upgrade', (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, socket => {
-    wsServer.emit('connection', socket, request);
-  });
-});
 app.use("", router)
