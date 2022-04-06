b57fbe0a359db7f80494a9134be43fc7aa5bc25d
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2022-04-06 14:56:46 -0700

    handle db restarts

--- src/services/database.service.js
+++ src/services/database.service.js
@@ -1,20 +1,35 @@
 const { Client } = require('pg');
 
-const dbclient = new Client({
-	host: process.env.DATABASE_HOST || '',
-	user: process.env.DATABASE_USER || '',
-	password: process.env.DATABASE_PASSWORD || '',
-	database: process.env.DATABASE_NAME || '',
-});
+function connect() {
+	var dbclient = new Client({
+		host: process.env.DATABASE_HOST || '',
+		user: process.env.DATABASE_USER || '',
+		password: process.env.DATABASE_PASSWORD || '',
+		database: process.env.DATABASE_NAME || '',
+	});
 
-dbclient.connect().then(() => {
-	console.log('setup listener');
-	dbclient.query( 'LISTEN guests' )
-	.then(result => console.log(result))
-	.catch(e => console.error("ERROR: ",e.stack))
-	dbclient.query( 'LISTEN announcements' )
-	.then(result => console.log(result))
-	.catch(e => console.error("ERROR: ",e.stack))
-});
+    dbclient.connect().then(() => {
+		dbclient.query( 'LISTEN guests' )
+		.then(result => console.log(result))
+		.catch(e => console.error("ERROR: ",e.stack))
+		dbclient.query( 'LISTEN announcements' )
+		.then(result => console.log(result))
+		.catch(e => console.error("ERROR: ",e.stack))
+	}).catch(e => {
+		console.error(e.stack)
+		setTimeout(() => {
+			console.log("...Reconnecting")
+			connect()
+		}, 1000);
+	  })
+
+    dbclient.on('error', error => {
+		console.error("Error with DB connection", error.stack)
+        connect();
+    });
+	return dbclient
+}
+
+var dbclient = connect()
 
 module.exports = dbclient;
