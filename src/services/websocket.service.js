d0844d6670a607ea418b0d9d53fba440b98423fa
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2023-06-20 14:02:16 -0700

    unclaim guests on agent disconnect

--- src/services/websocket.service.js
+++ src/services/websocket.service.js
@@ -72,6 +72,8 @@ wss.on('connection', function connection( ws, req ) {
 				if( agent ){
 					ws.id = agent.id;
 					await apiService.updateAgent( ws.id, { status: 'disconnected' } );
+					console.log("unclaiming guests")
+					await apiService.unclaimGuests( ws.id );
 				} else {
 					ws.terminate()
 				}
